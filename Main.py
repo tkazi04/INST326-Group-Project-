278371281c10fd2a6bf58158fe54d507490e69c4
tkazi04 <tkazi04@terpmail.umd.edu>
2025-11-22 12:08:18 -0500

    Added Main.py file

---
+++ Main.py
@@ -0,0 +1,16 @@
+"""
+    This file allows users to:
+    - Add new tasks
+    - List all tasks
+    - Complete a task
+    - Edit task information
+    - Remove a task
+    - Search tasks by keyword
+    - Save tasks to JSON
+    - Load tasks from JSON
+
+It interacts with TaskManager, which manages storage,
+searching, filtering, sorting, and persistence of Task objects.
+"""
+
+from TaskManager import TaskManager
